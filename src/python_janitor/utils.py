fce7a0e29eab617926f3338925f7157703b6885e
pymk <pymk@users.noreply.github.com>
2026-02-21 19:15:41 -0800

    move string consts to strings.py; delete empty utils.py file

--- src/python_janitor/utils.py
+++
@@ -1,3 +0,0 @@
-ALPHA_NUMERIC_SPACES = r"[^a-zA-Z0-9 ]+"
-LATIN = "ä  æ  ǽ  đ ð ƒ ħ ı ł ø ǿ ö  œ  ß  ŧ ü  Ä  Æ  Ǽ  Đ Ð Ƒ Ħ I Ł Ø Ǿ Ö  Œ  ẞ  Ŧ Ü "
-ASCII = "ae ae ae d d f h i l o o oe oe ss t ue AE AE AE D D F H I L O O OE OE SS T UE"
